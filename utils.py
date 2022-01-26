f149ef9f9c2674b7c0b3a0438c5ce503430cf318
Marik28 <markel20002000@gmail.com>
2022-01-26 22:55:23 +0600

    rename param

--- utils.py
+++ utils.py
@@ -4,16 +4,16 @@ from typing import Iterable
 import numpy as np
 
 
-def find_nearest(array: Iterable[Number], value: Number) -> Number:
+def find_nearest(values: Iterable[Number], value: Number) -> Number:
     """
     >>> find_nearest([1, 2.1, 2.12, 2.15, 5], 2.13)
     2.12
 
-    :param array:
+    :param values:
     :param value:
     :return: nearest number to given value in given array
     """
-    array = np.asarray(array)
+    array = np.asarray(values)
     idx = np.abs(array - value).argmin()
     return array[idx]
 
