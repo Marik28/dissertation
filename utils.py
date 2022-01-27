3f408a891176f0105c422742ceac073d4fec7cfe
Marik28 <markel20002000@gmail.com>
2022-01-27 09:56:16 +0600

    add fill_empty

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from numbers import Number
 from typing import Iterable
 
 import numpy as np
+import pandas as pd
 
 
 def find_nearest(values: Iterable[Number], value: Number) -> Number:
@@ -16,6 +17,23 @@ def find_nearest(values: Iterable[Number], value: Number) -> Number:
     array = np.asarray(values)
     idx = np.abs(array - value).argmin()
     return array[idx]
+
+
+def fill_empty(data: pd.DataFrame) -> dict:
+    data_as_dict = {int(row["T"]): row["R"] for _, row in data.iterrows()}
+    temperatures = list(data_as_dict.keys())
+    new_values = {temp: None for temp in range(min(temperatures), max(temperatures) + 1)}
+    for temp in sorted(list(new_values)):
+        res = data_as_dict.get(temp)
+        new_values[temp] = res
+    for temp in sorted(list(new_values)):
+        res = new_values.get(temp)
+        if res is None:
+            prev_r = new_values[temp - 1]
+            next_r = new_values[temp + 1]
+            mean = round((prev_r + next_r) / 2, 3)
+            new_values[temp] = mean
+    return new_values
 
 
 if __name__ == '__main__':
