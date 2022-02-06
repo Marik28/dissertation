f1e8b3900b42019c0774ae957a0aa534ca35c89e
Marik28 <69481006+Marik28@users.noreply.github.com>
2022-02-06 18:59:33 +0600

    add db setting

--- src/dissertation_gui/settings.py
+++ src/dissertation_gui/settings.py
@@ -5,7 +5,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     base_dir: Path = Path(__file__).parent.parent
+    db_url = f'sqlite:///{base_dir / "dissertation_gui" / "db.sqlite3"}'
 
 
 settings = Settings()
-print(settings.base_dir)
