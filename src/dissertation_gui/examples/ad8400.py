e67f39da9138be55ee4a27171779b11866beaa2c
Marik28 <69481006+Marik28@users.noreply.github.com>
2022-02-20 20:46:01 +0600

    remove logging

--- src/dissertation_gui/examples/ad8400.py
+++ src/dissertation_gui/examples/ad8400.py
@@ -10,10 +10,7 @@ ad8400 = AD8400(clock=sclk, mosi=mosi, cs=cs)
 
 if __name__ == '__main__':
     import time
-    from loguru import logger
 
-    logger.info(f"Инициализирован ad8400 с портами {sclk=}, {mosi=}, {cs=}")
     for i in range(ad8400.min_code, ad8400.max_code + 1):
         ad8400.send_code(i)
-        logger.info(f"Отправляется код {i}")
         time.sleep(2)
