55850f101f377c5a1ed7b1ea27946a532a945528
Marik28 <69481006+Marik28@users.noreply.github.com>
2022-02-20 18:43:12 +0600

    add mcp_4725.py

---
+++ src/dissertation_gui/devices/mcp_4725.py
@@ -0,0 +1,22 @@
+import microcontroller
+from busio import I2C
+
+from .base import BaseDevice
+
+
+class MCP4725(BaseDevice):
+    min_code = 0
+    max_code = 4095
+
+    def __init__(self, scl: microcontroller.Pin, sda: microcontroller.Pin, address: int):
+        self._i2c = I2C(scl, sda)
+        self._address = address
+
+    def _perform_send_data(self, data: bytes) -> None:
+        self._i2c.writeto(self._address, data)
+
+    def _begin_transaction(self):
+        while not self._i2c.try_lock():
+            pass
+        yield
+        self._i2c.unlock()
