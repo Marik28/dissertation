9382a7e9b4b24cabbd944d0c00169c40413ce2be
Marik28 <markel20002000@gmail.com>
2022-03-09 13:00:26 +0600

    add pymodbus

---
+++ src/dissertation_gui/threads/modbus.py
@@ -0,0 +1,33 @@
+import time
+
+from PyQt5.QtCore import QThread
+from pymodbus.client.sync import BaseModbusClient
+
+
+# TODO выбрать параметры для отслеживания
+#   - Измеренная величина
+#   - Тип входного датчика или сигнала
+#   - Постоянная времени цифрового фильтра
+#   - Способ управления для выхода (холодильник/нагреватель)
+#   - Уставка
+
+class ThermoRegulatorInfoThread(QThread):
+    """Поток для периодического опроса ТРМ-а"""
+
+    def __init__(self, client: BaseModbusClient, frequency: int = 1, parent=None):
+        """
+
+        :param client: инициализированный Modbus клиент
+        :param frequency: частота опросов ТРМ-а (Гц)
+        :param parent:
+        """
+        super().__init__(parent)
+        self._client = client
+        self._update_period = 1 / frequency
+
+    def run(self) -> None:
+        while True:
+            # TODO реализовать
+            with self._client:
+                pass
+            time.sleep(self._update_period)
