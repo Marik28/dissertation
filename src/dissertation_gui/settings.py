01e2d6365951eb786def8bc415791da9447741db
Marik28 <markel20002000@gmail.com>
2022-04-13 17:29:42 +0600

    add .env.example; add settings

--- src/dissertation_gui/settings.py
+++ src/dissertation_gui/settings.py
@@ -9,12 +9,27 @@ base_dir: Path = Path(__file__).parent.parent
 class Settings(BaseSettings):
     base_dir: Path = base_dir
     db_url = f'sqlite:///{base_dir / "dissertation_gui" / "db.sqlite3"}'
+
     plot_update_frequency: int = 10
-    plot_points: int = 1000
+    plot_points: int
+
+    # настройки протокола OWEN
     port: str
-    baudrate: int = 19200
+    baudrate: int
     trm_address: int = 1
+
+    # настройки протокола I2C
     mcp4725_address: int
+
+    # пины - как они названы в библиотеке board
+    relay_1_pin: str
+    relay_2_pin: str
+    relay_3_pin: str
+    relay_4_pin: str
+    cs0_pin: str
+    cs1_pin: str
+    i2c_sda_pin: str
+    i2c_scl_pin: str
 
 
 settings = Settings(_env_file=base_dir.parent / ".env")
