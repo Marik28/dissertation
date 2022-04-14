2fef5f361d9603ca512207c60f55508957ead540
Марк Фицек <markel20002000@gmail.com>
2022-04-14 17:30:00 +0600

    fixes

---
+++ src/dissertation_gui/devices/__init__.py
@@ -0,0 +1,3 @@
+from .ad8400 import AD8400
+from .mcp_4725 import MCP4725
+from .relay import DigitalIORelay
--- src/scripts/test_mcp4725.py
+++ src/scripts/test_mcp4725.py
@@ -1,11 +1,32 @@
+import board
 import typer
+from busio import I2C
+
+from dissertation_gui.devices import (
+    MCP4725,
+    DigitalIORelay,
+)
+from dissertation_gui.settings import settings
 
 app = typer.Typer()
+
+mcp4725 = MCP4725(
+    I2C(getattr(board, settings.i2c_scl_pin), getattr(board, settings.i2c_sda_pin)),
+    settings.mcp4725_address,
+)
+relay_1 = DigitalIORelay(getattr(board, settings.relay_1_pin))
+relay_2 = DigitalIORelay(getattr(board, settings.relay_2_pin))
+relay_3 = DigitalIORelay(getattr(board, settings.relay_3_pin))
+relay_4 = DigitalIORelay(getattr(board, settings.relay_4_pin))
+
+
+def configure_relays():
+    pass
 
 
 @app.command
 def main():
-    pass
+    configure_relays()
 
 
 if __name__ == '__main__':
