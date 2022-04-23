fd96acc4461a871138f5751fcca7daa629dca2cf
Marik28 <markel20002000@gmail.com>
2022-04-23 11:02:30 +0600

    change spi

--- src/scripts/test_ad8400.py
+++ src/scripts/test_ad8400.py
@@ -2,16 +2,16 @@ import time
 
 import board
 import typer
-from adafruit_extended_bus import ExtendedSPI as SPI
+from busio import SPI
 
 from dissertation_gui.devices.ad8400 import AD8400
 from dissertation_gui.devices.relay import DigitalIORelay
 from dissertation_gui.settings import settings
 
 app = typer.Typer()
-
-ad8400_1 = AD8400(SPI(1, 0), getattr(board, settings.cs0_pin))
-ad8400_2 = AD8400(SPI(1, 2), getattr(board, settings.cs1_pin))
+spi = SPI(clock=board.SCLK, MOSI=board.MOSI, MISO=None)
+ad8400_1 = AD8400(spi, getattr(board, settings.cs0_pin))
+ad8400_2 = AD8400(spi, getattr(board, settings.cs1_pin))
 relay_1 = DigitalIORelay(getattr(board, settings.relay_1_pin))
 relay_2 = DigitalIORelay(getattr(board, settings.relay_2_pin))
 relay_3 = DigitalIORelay(getattr(board, settings.relay_3_pin))
