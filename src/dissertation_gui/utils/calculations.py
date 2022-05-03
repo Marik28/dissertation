6b0dbe57c681b44943e3cdfee1eb8091499a9657
Marik28 <markel20002000@gmail.com>
2022-05-03 17:48:42 +0600

    describe solver

--- src/dissertation_gui/utils/calculations.py
+++ src/dissertation_gui/utils/calculations.py
@@ -2,11 +2,36 @@ from abc import abstractmethod, ABCMeta
 from numbers import Number
 
 
-class Solver(ABCMeta):  # TODO вынести расчёты температуры из потока в этот класс
+class Solver(metaclass=ABCMeta):  # TODO вынести расчёты температуры из потока в этот класс
 
     @abstractmethod
-    def calculate_temperature(self) -> float:
+    def reset_time(self):
         pass
 
-    def set_temperature(self, new_temperature: Number):
-        ...
+    @abstractmethod
+    def set_temperature(self, temperature: Number):
+        pass
+
+    @abstractmethod
+    def set_interference_amplitude(self, amplitude: Number):
+        pass
+
+    @abstractmethod
+    def set_interference_frequency(self):
+        pass
+
+    @abstractmethod
+    def calculate_temperature(self, time: Number) -> Number:
+        pass
+
+    @abstractmethod
+    def calculate_burst_noise(self) -> Number:
+        pass
+
+    @abstractmethod
+    def calculate_sinusoidal_noise(self) -> Number:
+        pass
+
+
+class LinearSolver(Solver):
+    pass
