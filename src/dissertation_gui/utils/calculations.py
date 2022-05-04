6f959e8a361e3abcadcca90f2672c71d7d6191dd
Марк Фицек <markel20002000@gmail.com>
2022-05-04 14:12:35 +0600

    implementing

--- src/dissertation_gui/utils/calculations.py
+++ src/dissertation_gui/utils/calculations.py
@@ -1,37 +1,45 @@
+import math
 from abc import abstractmethod, ABCMeta
-from numbers import Number
 
 
 class Solver(metaclass=ABCMeta):  # TODO вынести расчёты температуры из потока в этот класс
 
-    @abstractmethod
-    def reset_time(self):
-        pass
+    def __init__(self,
+                 k: float,
+                 start_temperature: float,
+                 interference_amplitude: float,
+                 interference_frequency: float,
+                 set_temperature: float):
+        self.k = k
+        self.start_temperature = start_temperature
+        self.temperature = start_temperature
+        self.interference_amplitude = interference_amplitude
+        self.interference_frequency = interference_frequency
+        self.set_temperature = set_temperature
+        self.direction = 1
+
+    def set_set_temperature(self, temperature: float):
+        self.set_temperature = temperature
+
+    def set_interference_amplitude(self, amplitude: float):
+        self.interference_amplitude = amplitude
+
+    def set_interference_frequency(self, frequency: float):
+        self.interference_frequency = frequency
 
     @abstractmethod
-    def set_temperature(self, temperature: Number):
+    def calculate_temperature(self, time: float) -> float:
         pass
 
-    @abstractmethod
-    def set_interference_amplitude(self, amplitude: Number):
+    def calculate_burst_noise(self) -> float:
         pass
 
-    @abstractmethod
-    def set_interference_frequency(self):
-        pass
-
-    @abstractmethod
-    def calculate_temperature(self, time: Number) -> Number:
-        pass
-
-    @abstractmethod
-    def calculate_burst_noise(self) -> Number:
-        pass
-
-    @abstractmethod
-    def calculate_sinusoidal_noise(self) -> Number:
-        pass
+    def calculate_sinusoidal_noise(self, time: float) -> float:
+        return self.interference_amplitude * math.sin(2 * math.pi * self.interference_frequency * time)
 
 
 class LinearSolver(Solver):
-    pass
+
+    def calculate_temperature(self, time: float) -> float:
+        self.direction = int(math.copysign(1, self.set_temperature - self.temperature))
+        return self.start_temperature + self.k * time * self.direction
